d82f7b7f89242f8901356f29c8479afad1336932
penguinbigwave <kmbigwave1114@gmail.com>
2016-10-10 11:58:12 +0900

    Update kadai1.m

--- kadai1.m
+++ kadai1.m
@@ -15,21 +15,21 @@ IMG2 = imresize(IMG,2,'box'); % 画像の拡大
 imagesc(IMG2); axis image; % 画像の表示
 pause; % 一時停止
 
-IMG = imresize(IMG2,0.5); % 画像の縮小
-IMG2 = imresize(IMG,4,'box'); % 画像の拡大
+IMG = imresize(IMG2,0.5); % 画像の縮小 % IMG2と直したが，0.5→0.25に変更でもいいかも．
+IMG2 = imresize(IMG,4,'box'); % 画像の拡大 % 0.25に直した場合は，4→2に変更する必要あるかも．
 imagesc(IMG2); axis image; % 画像の表示
 pause; % 一時停止
 
-IMG = imresize(IMG2,0.5); % 画像の縮小
+IMG = imresize(IMG2,0.5); % 画像の縮小 % 上記と同じ．
 IMG2 = imresize(IMG,8,'box'); % 画像の拡大
 imagesc(IMG2); axis image; % 画像の表示
 pause; % 一時停止
 
-IMG = imresize(IMG2,0.5); % 画像の縮小
+IMG = imresize(IMG2,0.5); % 画像の縮小 % 上記と同じ．
 IMG2 = imresize(IMG,16,'box'); % 画像の拡大
 imagesc(IMG2); axis image; % 画像の表示
 pause; % 一時停止
 
-IMG = imresize(IMG2,0.5); % 画像の縮小
+IMG = imresize(IMG2,0.5); % 画像の縮小 % 上記と同じ．
 IMG2 = imresize(IMG,32,'box'); % 画像の拡大
 imagesc(IMG2); axis image; % 画像の表示
