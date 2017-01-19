987a0b01a11e65127414b312334b47afaf5f3057
penguinbigwave <kmbigwave1114@gmail.com>
2017-01-19 14:29:43 +0900

    Update kadai6.m

--- kadai6.m
+++ kadai6.m
@@ -4,7 +4,7 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
-ORG=imread('https://www.pakutaso.com/shared/img/thumb/IS107140615_TP_V.jpg'); % 原画像の入力
+ORG=imread('penguin.png'); % 原画像の入力
 ORG = rgb2gray(ORG);
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause; % 一時停止
